99c8e52e2d3ab9b0dd10341f83d851c330fde081
noqcks <benji@093b.org>
2024-04-07 19:12:25 +0000

    Upgrade dependencies

--- src/TailwindClient.ts
+++ src/TailwindClient.ts
@@ -71,7 +71,7 @@ export class TailwindClient {
     return importFrom(tailwindRootPath, "postcss") as typeof import("postcss");
   }
 
-  private extractClassNames([base, components, utilities]: (Result | { root: Document })[]) {
+  private extractClassNames([base, components, utilities]: (Result | { root: Document | Root })[]) {
     const { classNames } = extractClassNames([
       { root: base.root, source: "base" },
       { root: components.root, source: "components" },
